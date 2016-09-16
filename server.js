865c1e4c560b976a4bb0df39b0928b136a5169f9
deeskr <deeskr@users.noreply.github.com>
2016-09-16 16:02:18 -0400

    implement avatar capability

--- server.js
+++ server.js
@@ -1,28 +1,35 @@
-/* 
+/*******************************************************************
  * Implemntation of multi-user X3DOM - server side.
  * author: Matthew Bock
-* This version focuses on minimizing data transfer, but it still
+ * This version focuses on minimizing data transfer, but it still
  * sends and receives updates as soon as they happen with no regard
  * to time since the last update.
- */
+ *
+ * edited by: Karsten Dees, Nick Hu {09/16/2016}
+ *******************************************************************/
 
+//-----------------------------
+// Data Fields
+//-----------------------------
+ 
 var app = require('http').createServer(handler)
   , io = require('socket.io').listen(app)
   , fs = require('fs')
   , url = require('url')
 
-var users = {};
-
-//io.set("log level", 2);
+var users = {}; //List of Connected Users
 
 app.listen(8888);
 
+/*
+ * Handle incorrect Path Names for the server connection
+ *
+ * @param req - Data included in request (IP Address, HTTP headers, url, etc.)
+ * @param res - Data sent back to browser from server
+ */
 function handler (req, res) 
 {
   var pathname = url.parse(req.url).pathname;
-   // res.write('Hello World');
-   // res.end();
-   // return;
   fs.readFile(__dirname + pathname,
   function (err, data) {
     if (err) {
@@ -32,22 +39,39 @@ function handler (req, res)
 
     res.writeHead(200);
     res.end(data);
-	//res.close();
   });
 }
 
-io.sockets.on('connection', function (socket)
-{ // Define socket events
-  console.log("new connection");
+/*
+ * Socket Connection and defined socket events
+ *
+ * @param socket - the connected socket
+ */
+io.on('connection', function (socket)
+{ 
+
+ /*
+  * Recieved when a new client opens a websocket connection succesfully
+  */
   socket.on('newconnection', function()
-    {
-      // Recieved when a new client opens a websocket connection succesfully.
-      // Add the new client to the list of all clients.
-      console.log("New user is connecting...");
-
-      socket.emit('firstupdate', users);
-    })
-
+  {
+	// Add the new client to the list of all clients
+	console.log("New user is connecting...");
+	try {
+		io.emit('firstupdate', users);
+    } catch (e) {
+		console.log(e);
+	}
+  });
+ 
+ /*
+  * Recieved when a new client has successfully updated
+  * for the first time
+  *
+  * @param name - client's username
+  * @param pos - client's start position in the scene
+  * @param rot - client's start rotation in the scene
+  */
   socket.on('login', function(name, pos, rot)
   {
     console.log("New user '" + name + "' has connected.");
@@ -57,33 +81,47 @@ io.sockets.on('connection', function (socket)
     users[name] = [name, pos, rot];
 
     // Inform all clients to update and account for the new user.
-    io.sockets.emit('newuser', users[name]);
+	try {
+		io.emit('newuser', users[name], users);
+	} catch (e) {
+		console.log(e);
+	}
   });
 
+ /*
+  * Recieved when the client has changed the position
+  * of their avatar
+  *
+  * @param name - client's username
+  * @param pos - client's start position in the scene
+  * @param rot - client's start rotation in the scene
+  */
   socket.on('updateposition', function(name, pos, rot)
   {
-    //Recieved when one of the clients moves.
-    //console.log("User '" + name + "' moved to:" + pos.x + ", " + pos.y + ", " + pos.z + "; "
-    //                           + rot[0].x + ", " + rot[0].y + ", " + rot[0].z + ", " + rot[1]);
-
     // Update the master list with the client's new location.
     users[name] = [name, pos, rot];
 
-    // Inform all clients to update with the new list.
-    io.sockets.emit('update', users[name]);
+    // Inform all clients to update their scenes
+	try {
+		io.emit('update', users[name]);
+	} catch (e) {
+		console.log(e);
+	}
   });
 
+ /*
+  * Recieved when a client disconnects (closes their browser window/tab).
+  *
+  * @param name - client's username
+  * @param pos - client's start position in the scene
+  * @param rot - client's start rotation in the scene
+  */
   socket.on('disconnect', function()
   {
-    // Recieved when a client disconnects (closes their browser window/tab).
-    console.log("User '" + socket.username + "' disconnected");
-
-    // Inform all clients to update and account for the removed user.
-    io.sockets.emit('deleteuser', users[socket.username]);
-
-    // Remove the client from the master list
-    delete users[socket.username];
+	  // Inform all clients to update and account for the removed user.
+	  io.emit('deleteuser', users[socket.username]);
+	  
+	  // Remove the client from the master list
+	  delete users[socket.username];
   });
-  
-
 });
